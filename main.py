8481ca845a703d723627f8c5350f9d7738314a6e
amshaff <amshaffso@gmail.com>
2024-03-07 11:21:25 +0300

    Dobavil komm

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-#OPA OPA ZELEnAYA OY
+#OPA OPA ZELEnAYA OYa
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
